96834310517847226e3db1b572c46647e7ccbe94
Nayyir Jutha <nayyir.jutha@gmail.com>
2015-07-20 15:01:57 +0000

    bare workings of music wall

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,31 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do
+  @songs = Song.all
+  erb :'songs/index'
+end
+
+get '/songs/tracks' do
+  erb :'songs/tracks'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/details'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url:  params[:url]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/tracks'
+  end
+end
+
